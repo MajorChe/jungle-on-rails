625f98eded9ad472411993db55f8f0171ebc1b08
charit nanduri <charithnanduri03@gmail.com>
2022-01-11 22:49:16 +0000

    add fetaure specs to home page

---
+++ spec/features/home_page_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+  
+  #set up products
+  before :each do
+    @category = Category.create! name: 'Apparel'
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+  scenario "They see all products" do
+    #Act
+    visit root_path
+    #debug
+    save_screenshot
+    expect(page).to have_css 'article.product', count: 10
+  end
+end
