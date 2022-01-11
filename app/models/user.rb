9face1eb50d32479e5c3b998a4ade0dfc7564f07
charit nanduri <charithnanduri03@gmail.com>
2022-01-11 05:43:11 +0000

    add user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,15 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password, presence: true, length: { minimum: 2 }
+  validates :password_confirmation, presence: true, length: { minimum: 2 }
 
-  def self.authenticate_with_credentials
-    email: email.downcase.strip
+  def self.authenticate_with_credentials(email, password)
+    email = email.downcase.strip
     user = User.find_by_email(email)
     if user && user.authenticate(password)
-      user
-    else
-      nil
+      return user
     end
+    nil
   end
+  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,28 +3,47 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "User Validations" do
     it "password and confirm passoword should be the same" do
-      @user = User.create(email: "a@a.com",password: "a",password_confirmation: "a")
+      @user = User.new(email: "a@a.com",password: "a",password_confirmation: "a")
       expect(@user.password).to eq(@user.password_confirmation)
     end
 
     it "password is required" do
-      @user = User.create(email: "a@a.com",password: nil, first_name: "a", last_name: "b")
+      @user = User.new(email: "a@a.com",password: nil, first_name: "a", last_name: "b")
       expect(@user).to_not be_valid
     end
 
     it "emails must be unique" do
-      @user1 = User.create(email: "a@a.COM", password: 'a', first_name: "a", last_name: "b")
-      @user2 = User.create(email: "A@A.com", password: 'b', first_name: "a", last_name: "b")
+      @user1 = User.new(email: "a@a.COM", password: 'a', first_name: "a", last_name: "b")
+      @user2 = User.new(email: "A@A.com", password: 'b', first_name: "a", last_name: "b")
       expect(@user2).to_not be_valid
     end
 
     it "password has a minimum length of 2" do
-      @user = User.create(email: "a@a.com",password: '1', first_name: "a", last_name: "b")
+      @user = User.new(email: "a@a.com",password: '1', first_name: "a", last_name: "b")
       expect(@user).to_not be_valid
     end
   end
 
-  describe ".authenticate_with_credentials" do
-    
+  describe '.authenticate_with_credentials' do
+
+    it ("should accept email and password as arguments in the login form") do
+      @user1 = User.new(first_name: "a", last_name: "b", email: "a@a.com", password: "123", password_confirmation: "123")
+      @user2 = User.authenticate_with_credentials("b@a.com", "123")
+      expect(@user2).not_to eq(@user1)
+    end
+
+    it ("should not be case sensitive for email, all emails, to_lowercase.") do
+      @user1 = User.new(first_name: "b", last_name: "d", email: "b@d.com", password: "123", password_confirmation: "123")
+      @user1.save!
+      @user2 = User.authenticate_with_credentials("b@d.com", "123")
+      expect(@user2).to eq(@user1)
+    end
+
+    it ("should not be case sensitive for email, all emails, to_lowercase.") do
+      @user1 = User.new(first_name: "c", last_name: "e", email: "abcd@xyz.com", password: "123", password_confirmation: "123")
+      @user1.save!
+      @user2 = User.authenticate_with_credentials("ABCD@XYZ.com", "123")
+      expect(@user2).to eq(@user1)
+    end
   end
 end
