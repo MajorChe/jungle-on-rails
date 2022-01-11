c64f810b0d8f13d218a210c3dbf11adb37d80674
charit nanduri <charithnanduri03@gmail.com>
2022-01-11 05:21:19 +0000

    add authentication to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,15 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: { case_sensitive: false }
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 2 }
+
+  def self.authenticate_with_credentials
+    email: email.downcase.strip
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,9 +13,18 @@ RSpec.describe User, type: :model do
     end
 
     it "emails must be unique" do
-      @user1 = User.create(email: "a@a.COM", password: 'a')
-      @user2 = User.create(email: "A@A.com", password: 'b')
+      @user1 = User.create(email: "a@a.COM", password: 'a', first_name: "a", last_name: "b")
+      @user2 = User.create(email: "A@A.com", password: 'b', first_name: "a", last_name: "b")
       expect(@user2).to_not be_valid
     end
+
+    it "password has a minimum length of 2" do
+      @user = User.create(email: "a@a.com",password: '1', first_name: "a", last_name: "b")
+      expect(@user).to_not be_valid
+    end
+  end
+
+  describe ".authenticate_with_credentials" do
+    
   end
 end
